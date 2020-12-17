0510579e0c3275fc7312988e99ae08e3889d3092
Alex Racinski <aracinski2@gmail.com>
2020-12-17 13:44:21 -0600

    add index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="place in places">
+      <h4>{{ place.name }}</h4>
+      <h5>{{ place.address }}</h5>
+    </div>
   </div>
 </template>
 
@@ -8,13 +12,34 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to Vue.js!!!!!!!!!!",
+      places: [],
+      name: "",
+      address: "",
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    console.log("in created");
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      console.log("places index");
+      axios.get("/api/places").then((response) => {
+        console.log(response.data);
+        this.places = response.data;
+      });
+    },
+    // createPlaces: function () {
+    //   console.log("create place");
+    //   axios.post("/api/places").then((response) => {
+    //     console.log();
+    //   });
+    // },
+  },
 };
 </script>
